b4429b01b25e344d7fbb97ec9624e23dbb5691ca
Jake Studer <jacob.studer.dev@gmail.com>
2022-08-28 22:59:02 -0600

    Created a user case for a simple update, added update

--- Updates, Deletes, Inserts/Update.sql
+++ Updates, Deletes, Inserts/Update.sql
@@ -1,1 +1,9 @@
 --Update file
+-- We have sale that was the incorrect amount that we are not able to update in the front end, we are receiving an error when trying to do so.
+--Please update orderID 75113 Subtotal to $234.29, make sure that all connecting tables are also updated.
+
+use AdventureWorks2012
+
+update Sales.SalesOrderHeader
+set SubTotal to 234.29
+where orderID = 75113
