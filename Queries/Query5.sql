cbf8b9008744a9876141dda8389be3b3a7505b0d
Jake Studer <jacob.studer.dev@gmail.com>
2022-11-23 14:07:13 -0700

    added query5

---
+++ Queries/Query5.sql
@@ -0,0 +1,20 @@
+--having clause
+
+--Starting with the Sales.SalesOrderHeader table, join to the Sales.SalesOrderDetail table. This
+--table contains the line item details associated with each sale. From Sales.SalesOrderDetail, join
+--to the Production.Product table. Return the Name column from Production.Product and assign
+--it the column alias “Product Name”. For each product, find out how many of each product was
+--ordered for all orders that occurred in 2006. Only output those products where at least 200
+--were ordered.
+
+SELECT
+P.Name AS [Product Name],
+SUM(SOD.OrderQty) AS ProductOrderCount
+FROM Sales.SalesOrderHeader SOH
+INNER JOIN Sales.SalesOrderDetail SOD
+ON SOD.SalesOrderID = SOH.SalesOrderID
+INNER JOIN Production.Product P
+ON P.ProductID = SOD.ProductID
+WHERE SOH.OrderDate BETWEEN '1/1/2006' AND '12/31/2006'
+GROUP BY P.Name
+HAVING SUM(SOD.OrderQty) >= 200
