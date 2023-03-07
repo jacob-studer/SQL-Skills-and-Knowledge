e101ff918264b423d34078fa58114b77dc9df05f
Jake Studer <jacob.studer.dev@gmail.com>
2023-03-07 10:02:52 -0700

    recfactored query 5 / queries folder

--- Queries/Query5.sql
+++ Queries/Query5.sql
@@ -1,13 +1,11 @@
---Starting with the Sales.SalesOrderHeader table, join to the Sales.SalesOrderDetail table. This
---table contains the line item details associated with each sale. From Sales.SalesOrderDetail, join
---to the Production.Product table. Return the Name column from Production.Product and assign
+--Return the Name column from Production.Product and assign
 --it the column alias “Product Name”. For each product, find out how many of each product was
 --ordered for all orders that occurred in 2006. Only output those products where at least 200
 --were ordered.
 
 SELECT
-P.Name AS [Product Name],
-SUM(SOD.OrderQty) AS ProductOrderCount
+    P.Name AS [Product Name],
+    SUM(SOD.OrderQty) AS ProductOrderCount
 FROM Sales.SalesOrderHeader SOH
 INNER JOIN Sales.SalesOrderDetail SOD
 ON SOD.SalesOrderID = SOH.SalesOrderID
